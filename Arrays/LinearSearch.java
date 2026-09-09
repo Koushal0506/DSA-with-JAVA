e0e2c2738e8f7780a826763245b668116e098d3e
Koushal Fand <koushal15156@gmail.com>
2026-09-10 00:28:54 +0530

    Initial commit

---
+++ Arrays/LinearSearch.java
@@ -0,0 +1,23 @@
+class LinearSearch {
+
+    public static int Linear_Search(int numbers[], int key){
+        for(int i=0;i<numbers.length;i++)
+            if(numbers[i]==key){
+                return i;
+            }
+            
+            return -1;
+    }
+    public static void main(String[] args) {
+        int numbers[]={2,4,6,8,10,12,14,16};
+        int key=10;
+
+        int index = Linear_Search(numbers, key);
+        if(index==-1){
+            System.out.println("Not Found");    
+        }
+        else{
+            System.out.println("Found 10 at index: "+index);
+        }
+    }    
+}
